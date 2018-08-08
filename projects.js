0a51fcf34b7ab303cd5be02ec5849cf41d2dcca7
Shing-Yun Chiang <shingyun>
2018-08-08 14:36:40 -0400

    update

---
+++ projects.js
@@ -0,0 +1,311 @@
+const projectData = [
+  { 
+    title: 'Poverty Levels and Floods',
+    img: 'media/project-images/poverty-levels-floods.png',
+    tags: ['Physical','Mapping'],
+    tools: ['QGIS','Illustrator','Laser cut'],
+    date: 'May 2018',
+    link: '',
+    path: 'PovertyLevelsAndFloods',
+    mainImg: 'media/page-images/poverty-and-flood-1.jpg',
+    intro: '', 
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ] 
+  },{
+    title: 'Network of Politicians and Banks',
+    img: 'media/project-images/pep-network.png',
+    tags: ['Static','Thomson Reuters'],
+    tools: ['Python','Gephi','Illustrator'],
+    date: 'Jul. 2018',
+    link: '',
+    path: 'NetworkOfPep',
+    mainImg: '',
+    intro: '',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]    
+  },{
+    title: 'World Cup Watchers',
+    img: 'media/project-images/wc-watchers.png',
+    tags: ['Static','Thomson Reuters'],
+    tools: ['D3','Illustrator'],
+    date: 'Jun. 2018',
+    link: 'https://blogs.thomsonreuters.com/answerson/where-are-people-watching-the-world-cup/',
+    path: 'WCWatchers',
+    mainImg: 'media/page-images/wc-watcher-1.jpg',
+    intro: '',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]  
+  },{
+    title: 'Income Disparity',
+    img: 'media/project-images/income-disparity.png',
+    tags: ['Interactive'],
+    tools: ['D3'],
+    date: 'Jul. 2018',
+    link: 'https://shingyun.github.io/income-disparity/',
+    path: 'IncomeDisparity',
+    mainImg: 'media/page-images/income-disparity-1.png',
+    intro: '',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]   
+  },{ 
+    title: 'Exonerations of Murder',
+    img: 'media/project-images/exoneration-murder.png',
+    tags: ['Interactive'],
+    tools: ['D3'],
+    date: 'Nov. 2017',
+    link: 'https://shingyun.github.io/exoneration-timeline/',
+    path: 'ExonerationsOfMurder',
+    mainImg: 'media/page-images/exoneration-murder-1.png',
+    intro: "This project focuses on the exonerations of murder, which is the main accused crime in the exoneration data, and visualizes how the wrongful convictions impacted these exonerees' lives. The impact of the wrongful conviction is demonstrated via the animating timeline, pointing out what year and age the exoneree was at.",
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]  
+  },{
+    title: 'After The Floods',
+    img: 'media/project-images/afterthefloods.png',
+    tags: ['Interactive','Graphicacy'],
+    tools: ['D3','OpenRefine','Tableau'],
+    date: 'Dec. 2017',
+    link: 'https://shingyun.github.io/afterthefloods/',
+    path: 'AfterTheFloods',
+    mainImg: 'media/page-images/after-the-floods-1.png',
+    intro: 'While I was interning at Graphicacy in Fall 2017, I analyzes 4,500 floods from 1984 to Sep. 2017 around the world, and showed relation between causes and impacts (e.g. deaths, people displaced) of floods historically and greographically.',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ] 
+  },{
+    title: '3D Poverty Map of Houston',
+    img: 'media/project-images/poverty-levels.png',
+    tags: ['Physical','Mapping'],
+    tools: ['QGIS','Illustrator','Laser cut'],
+    date: 'Mar. 2018',
+    link: '',
+    path: '3DPovertyMapOfHouston',
+    mainImg: 'media/page-images/3D-poverty-houston-1.JPG',
+    intro: 'This 3D portraits the elevation map of poverty in central part of Houston. The lower the area, the more poor people live there. The highest areas have a percentage of 0-10% population live under poverty line, the lowest areas have over 40% of population in poverty. The "holes" and the "hills" act as a metaphor of social hierarchy. Poor people — who are at a lower level in the society — are struggling in the holes, while the rich enjoy their life on the hills.',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]  
+  },{
+    title: 'Exonerations in The US',
+    img: 'media/project-images/exoneration.png',
+    tags: ['Static','Statistics'],
+    tools: ['D3','R','Illustrator'],
+    date: 'Apr. 2017',
+    link: '',
+    path: 'ExonerationsInTheUS',
+    mainImg: 'media/page-images/exonerations-1.png',
+    intro: 'This project explored the data set on exonerations in the US. The data set is provided by The National Registry of Exonerations. This project has examined the data from different aspects through descriptive statistic methods and finally focused on the time that exonerees have experienced from the accused crimes occurred to wrongful convictions and to exonerations. This project also explored the answers of the following questions by visualizations: Who are these exonerees? Where were they exonerated? How long have them been waiting for exonerations? Does DNA as the new evidence help? How the types of accused crimes influence exonerations?',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]  
+  },{
+    title: 'Journalist Deaths',
+    img: 'media/project-images/journalist-deaths.png',
+    tags: ['Interactive'],
+    tools: ['D3','R'],
+    date: 'Apr. 2017',
+    link: 'https://shingyun.github.io/JournalistsKilled/',
+    path: 'JournalistDeaths',
+    mainImg: 'media/page-images/journalist-deaths-1.png',
+    intro: 'This project visualized each journalist death through years, countries they were killed and causes, with the connection to historical events. The data set is from Committee to Protect Journalists(CPJ). There were more than 1700 journalists and media workers killed over the world from 1992 to April 2017 (when I downloaded the data set), this project analyzed 1232 deaths which are motive confirmed.',
+    paras: [
+      { subTitle: 'Design Decisions',
+        subParas:
+          { 
+            des: 'This data set caught my attention because I am also a journalist. I am interested in where and when the journalists were killed as well as what caused their deaths. The stories behind this are also important and will make this visualization more meaningful.  Therefore, I decided to make every journalist as an entity, represented by a circle. I hope readers would view a data point as a person, instead of merely number. When hover over a circle, there will be a short description about the journalist in order to bond the readers and data.',
+            imgs: [
+              { 
+                img: 'media/page-images/journalist-deaths-3.png',
+                caption: 'The reason why the journalists were killed. This visualization also shows the background information of journalists when users hover over a circle.'
+              }
+            ]
+          }
+      },//subTitle
+      { subTitle: 'Data Insights',
+        subParas: 
+          {
+            des: 'I explored the data with R and made some simple bar charts to get a sense of how this data looks like. Firstly I took a look at the top 20 countries where the journalists were murdered. It is not surprised that the top 1 country was Iraq, but the second country was the Philippines. As I know, the Philippines does not have war frequently, why it ranked 2? Then I focused on the deaths within the Philippines and looked at the data by year. I found that the was a huge amount of deaths in 2009, it’s when the Maguindanao Massacre happened. 30 journalists died during the tragedy. Therefore I came up with an idea to show the connection between journalist deaths and historical events that occurred in the top 20 countries.',
+            imgs: [
+              {
+                img: 'media/page-images/journalist-deaths-5.png',
+                caption: 'The top 20 countries where journalists were killed.'
+              },{
+                img: 'media/page-images/journalist-deaths-6.png',
+                caption: 'There was an unusual number of deaths in 2009 in Philippines.'               
+              },{
+                img: 'media/page-images/journalist-deaths-2.png',
+                caption: 'There is an event bar at the bottom, when hover over an event to show the connection to the deaths.'
+              }
+            ]
+          }
+      },//subTitle
+      { subTitle: 'Interactions',
+        subParas: 
+          {
+            des: 'This visualization has three views: Countries, Years, and Causes. It would be cool if we could have connection between views. Therefore, I made a feature to enable the connection. When readers click on a circle, it will select a group of circles of a country, year or cause depends on which view the readers are at. For example, if a reader clicks on a circle in Iraq under Countries view, a group of circles in Iraq will be highlighted. Then the reader switch to Years view, the selection is still active, and readers could know the journalists were killed in Iraq from a historical perspective.',
+            imgs: [
+              {
+                 img: 'media/page-images/journalist-deaths-4.png',
+                 caption: 'This view shows the distribution of journalist deaths in Iraq on a timeline.'
+              }
+            ]
+          }
+
+      }
+    ]   
+  },{
+    title: 'Fake News Sentiment',
+    img: 'media/project-images/fake-news.png',
+    tags: ['Interactive'],
+    tools: ['D3'],
+    date: 'Apr. 2017',
+    link: 'https://shingyun.github.io/SentimentViz/',
+    path: 'FakeNewsSentiment',
+    mainImg: 'media/page-images/fake-news-1.png',
+    intro: 'Americannews.com is identified as a fake news or satire website with over 5 millions followers on Facebook. The project analyzes sentiment of articles mentioned "Trump" and "Clinton" from the website using Python. The sentiment scores range from 1 (positive) to -1 (negative). The results of the site show most of the articles are scored between 0.5 ~ -0.5, which is not as dramatic as previous assumption of the project.',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]   
+  },{
+    title: 'Evictions in East Boston',
+    img: 'media/project-images/east-boston-eviction.png',
+    tags: ['Interactive','Mapping','Story'],
+    tools: ['D3','R','ScrollMagic'],
+    date: 'Feb. 2018',
+    link: 'https://web.northeastern.edu/visualizingeastie/court-documents-suggest-who-gets-evicted-and-why/',
+    path: 'EvictionsInEastBoston',
+    mainImg: 'media/page-images/east-boston-evictions-1.png',
+    intro: 'This project explores who are evicted and reasons in East Boston. Since there is no data on the background of people who face eviction, this project plots every eviction on a map and compare with income and race data. The visual outcome shows that low-income areas and Latino communities have more evictions.',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]   
+  },{
+    title: 'Crimes in Washington, DC',
+    img: 'media/project-images/crime-dc.png',
+    tags: ['Interactive','Mapping','Story'],
+    tools: ['D3','Leaflet','R'],
+    date: 'Aug. 2017',
+    link: 'https://shingyun.github.io/dc_crime_2016/',
+    path: 'CrimesInDC',
+    mainImg: 'media/page-images/crime-dc-1.png',
+    intro: 'This personal project explored the data set on crime in Washington DC of 2016, mapping out the crime-prone areas and time. The data set is from Washington DC government.',
+    paras: [
+      { subTitle: '',
+        subParas: [
+          { 
+            img: '',
+            des: ''
+          },{ 
+            img: '',
+            des: ''
+          }
+        ]
+      }
+    ]   
+  }
+]
--- script.js
+++ script.js
@@ -1,147 +1,6 @@
 const filterData = 
   ['All', 'Physical', 'Interactive', 'Mapping','Static', 'Story']
 
-const projectData = [
-  { 
-  	title: 'Poverty Levels and Floods',
-  	img: 'media/project-images/poverty-levels-floods.png',
-    tags: ['Physical','Mapping'],
-    tools: ['QGIS','Illustrator','Laser cut'],
-    date: 'May 2018',
-    link: '',
-    path: 'PovertyLevelsAndFloods',
-    mainImg: 'media/main-images/main-poverty-and-flood.jpg',
-    intro: '', 
-    paras: [
-      { img: '',
-        des: ''
-      }
-    ] 
-  },{
-  	title: 'Network of Politicians and Banks',
-  	img: 'media/project-images/pep-network.png',
-    tags: ['Static','Thomson Reuters'],
-    tools: ['Python','Gephi','Illustrator'],
-    date: 'Jul. 2018',
-    link: '',
-    path: 'NetworkOfPep',
-    mainImg: '',
-    intro: '',
-    paras: [''] 	
-  },{
-  	title: 'World Cup Watchers',
-  	img: 'media/project-images/wc-watchers.png',
-  	tags: ['Static','Thomson Reuters'],
-    tools: ['D3','Illustrator'],
-    date: 'Jun. 2018',
-    link: 'https://blogs.thomsonreuters.com/answerson/where-are-people-watching-the-world-cup/',
-    path: 'WCWatchers',
-    mainImg: 'media/main-images/main-wc-watcher.jpg',
-    intro: '',
-    paras: ['']  
-  },{
-  	title: 'Income Disparity',
-  	img: 'media/project-images/income-disparity.png',
-    tags: ['Interactive'],
-    tools: ['D3'],
-    date: 'Jul. 2018',
-    link: 'https://shingyun.github.io/income-disparity/',
-    path: 'IncomeDisparity',
-    mainImg: 'media/main-images/main-income-disparity.png',
-    intro: '',
-    paras: ['']  
-  },{ 
-  	title: 'Exonerations of Murder',
-  	img: 'media/project-images/exoneration-murder.png',
-    tags: ['Interactive'],
-    tools: ['D3'],
-    date: 'Nov. 2017',
-    link: 'https://shingyun.github.io/exoneration-timeline/',
-    path: 'ExonerationsOfMurder',
-    mainImg: 'media/main-images/main-exoneration-murder.png',
-    intro: "This project focuses on the exonerations of murder, which is the main accused crime in the exoneration data, and visualizes how the wrongful convictions impacted these exonerees' lives. The impact of the wrongful conviction is demonstrated via the animating timeline, pointing out what year and age the exoneree was at.",
-    paras: ['']  
-  },{
-  	title: 'After The Floods',
-  	img: 'media/project-images/afterthefloods.png',
-  	tags: ['Interactive','Graphicacy'],
-    tools: ['D3','OpenRefine','Tableau'],
-    date: 'Dec. 2017',
-    link: 'https://shingyun.github.io/afterthefloods/',
-    path: 'AfterTheFloods',
-    mainImg: 'media/main-images/main-after-the-floods.png',
-    intro: 'While I was interning at Graphicacy in Fall 2017, I analyzes 4,500 floods from 1984 to Sep. 2017 around the world, and showed relation between causes and impacts (e.g. deaths, people displaced) of floods historically and greographically.',
-    paras: ['']  
-  },{
-  	title: '3D Poverty Map of Houston',
-  	img: 'media/project-images/poverty-levels.png',
-  	tags: ['Physical','Mapping'],
-  	tools: ['QGIS','Illustrator','Laser cut'],
-    date: 'Mar. 2018',
-    link: '',
-    path: '3DPovertyMapOfHouston',
-    mainImg: 'media/main-images/main-3D-poverty-houston.JPG',
-    intro: 'This 3D portraits the elevation map of poverty in central part of Houston. The lower the area, the more poor people live there. The highest areas have a percentage of 0-10% population live under poverty line, the lowest areas have over 40% of population in poverty. The "holes" and the "hills" act as a metaphor of social hierarchy. Poor people — who are at a lower level in the society — are struggling in the holes, while the rich enjoy their life on the hills.',
-    paras: [''] 
-  },{
-  	title: 'Exonerations in The US',
-  	img: 'media/project-images/exoneration.png',
-  	tags: ['Static','Statistics'],
-    tools: ['D3','R','Illustrator'],
-    date: 'Apr. 2017',
-    link: '',
-    path: 'ExonerationsInTheUS',
-    mainImg: 'media/main-images/main-exonerations.png',
-    intro: 'This project explored the data set on exonerations in the US. The data set is provided by The National Registry of Exonerations. This project has examined the data from different aspects through descriptive statistic methods and finally focused on the time that exonerees have experienced from the accused crimes occurred to wrongful convictions and to exonerations. This project also explored the answers of the following questions by visualizations: Who are these exonerees? Where were they exonerated? How long have them been waiting for exonerations? Does DNA as the new evidence help? How the types of accused crimes influence exonerations?',
-    paras: ['']  
-  },{
-  	title: 'Journalist Deaths',
-  	img: 'media/project-images/journalist-deaths.png',
-  	tags: ['Interactive'],
-    tools: ['D3','R'],
-    date: 'Apr. 2017',
-    link: 'https://shingyun.github.io/JournalistsKilled/',
-    path: 'JournalistDeaths',
-    mainImg: 'media/main-images/main-journalist-deaths.png',
-    intro: 'This project visualized each journalist death through years, countries they were killed and causes, with the connection to historical events. The data set is from Committee to Protect Journalists(CPJ). There were more than 1700 journalists and media workers killed over the world from 1992 to present, this project analyzed 1232 deaths which are motive confirmed.',
-    paras: ['']  
-  },{
-  	title: 'Fake News Sentiment',
-  	img: 'media/project-images/fake-news.png',
-  	tags: ['Interactive'],
-    tools: ['D3'],
-    date: 'Apr. 2017',
-    link: 'https://shingyun.github.io/SentimentViz/',
-    path: 'FakeNewsSentiment',
-    mainImg: 'media/main-images/main-fake-news.png',
-    intro: 'Americannews.com is identified as a fake news or satire website with over 5 millions followers on Facebook. The project analyzes sentiment of articles mentioned "Trump" and "Clinton" from the website using Python. The sentiment scores range from 1 (positive) to -1 (negative). The results of the site show most of the articles are scored between 0.5 ~ -0.5, which is not as dramatic as previous assumption of the project.',
-    paras: ['']  
-  },{
-  	title: 'Evictions in East Boston',
-  	img: 'media/project-images/east-boston-eviction.png',
-   	tags: ['Interactive','Mapping','Story'],
-    tools: ['D3','R','ScrollMagic'],
-    date: 'Feb. 2018',
-    link: 'https://web.northeastern.edu/visualizingeastie/court-documents-suggest-who-gets-evicted-and-why/',
-    path: 'EvictionsInEastBoston',
-    mainImg: 'media/main-images/main-east-boston-evictions.png',
-    intro: 'This project explores who are evicted and reasons in East Boston. Since there is no data on the background of people who face eviction, this project plots every eviction on a map and compare with income and race data. The visual outcome shows that low-income areas and Latino communities have more evictions.',
-    paras: ['']  
-  },{
-  	title: 'Crimes in Washington, DC',
-  	img: 'media/project-images/crime-dc.png',
-  	tags: ['Interactive','Mapping','Story'],
-    tools: ['D3','Leaflet','R'],
-    date: 'Aug. 2017',
-    link: 'https://shingyun.github.io/dc_crime_2016/',
-    path: 'CrimesInDC',
-    mainImg: 'media/main-images/main-crime-dc.png',
-    intro: 'This personal project explored the data set on crime in Washington DC of 2016, mapping out the crime-prone areas and time. The data set is from Washington DC government.',
-    paras: ['']  
-  }
-]
-
-
 //Create a list of project 
 const vm_project = new Vue({
   el: '#app',
@@ -177,7 +36,9 @@ const vm_project = new Vue({
         x = (index)*100
         console.log('x',x)
         $('#project-box').css('transform','translateX(-'+ x +'%)')
-        $('#project-focus').css('visibility','visible');
+        $('#project-focus')
+        .css('visibility','visible')
+        .scrollTop(0);
         $('#section-info').css('opacity',0.3);
         $('#section-work').css('opacity',0.3);
 
